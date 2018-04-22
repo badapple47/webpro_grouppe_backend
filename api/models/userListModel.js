0b1fbe08a87131e04c24db4b381e578fc83e0c6e
Pathompong Chaisri <mutex@Pathompongs-MacBook-Pro.local>
2018-04-22 23:09:08 +0700

    before authen

---
+++ api/models/userListModel.js
@@ -0,0 +1,42 @@
+'use strict'
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+
+var UserSchema = new Schema({
+    contactId: {
+        type: String,
+        Required: 'Please enter'
+    },
+    firstName: {
+        type: String,
+        Required: 'Please enter'
+    },
+    lastName: {
+        type: String,
+        Required: 'Please enter'
+    },
+    mobileNo: {
+        type: String,
+        Required: 'Please enter'
+    },
+    //missed left last_name
+        email: {
+        type: String,
+        Required: 'Please enter'
+    },
+    facebook: {
+        type: String,
+        Required: 'Please enter'
+    },
+    imageUrl: {
+        type: String,
+        Required: 'Please enter'
+    },
+    createdDate:{
+        type: Date,
+        default :Date.now
+
+    },
+})
+
+module.exports = mongoose.model('Users', UserSchema)
